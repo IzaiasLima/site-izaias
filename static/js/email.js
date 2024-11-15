3ef26b809a9a861ecd63adb2dcc428c7219e39a1
Izaias Lima <izaiasmlima@gmail.com>
2024-11-15 00:34:01 -0300

    Adicionar foncionalidade ao form de contato

---
+++ static/js/email.js
@@ -0,0 +1,23 @@
+(function () {
+    emailjs.init({
+        publicKey: "53EtKuGY0c5TKn1ah",
+    });
+})();
+
+window.onload = function () {
+    const frmEmail = document.getElementById('contact-form')
+    frmEmail.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_izaias_com_br', 'template_izaias_com_br', '#contact-form').then(
+            (response) => {
+                frmEmail.reset();
+                alert("Obrigado. Retornaremos seu contato logo que for possível. ")
+                console.log('EMAIL SUCCESS!', response.status, response.text);
+            },
+            (error) => {
+                console.log('EMAIL SENDER FAILED: ', error);
+            },
+        );
+    });
+}
