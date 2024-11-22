9262db5eaaee3af0c251340cb31fd88123648435
Izaias Lima <izaiasmlima@gmail.com>
2024-11-22 20:44:37 -0300

    Adicionar toast no envio de mensagem

--- static/js/email.js
+++ static/js/email.js
@@ -11,13 +11,16 @@ window.onload = function () {
         frmEmail.addEventListener('submit', function (event) {
             event.preventDefault();
 
+
             emailjs.sendForm('service_izaias_com_br', 'template_izaias_com_br', '#contact-form').then(
                 (response) => {
                     frmEmail.reset();
-                    alert("Obrigado. Retornaremos seu contato logo que for possível. ")
-                    console.log('EMAIL SUCCESS!', response.status, response.text);
+                    showToast("Obrigado! Retornaremos seu contato logo que for possível.");
+                    console.log('EMAIL SUCCESS!');
                 },
                 (error) => {
+                    const msg = "Desculpe, não foi possível enviar sua mensagem. Tente novamente mais tarde.";
+                    showToast(msg, true);
                     console.log('EMAIL SENDER FAILED: ', error);
                 },
             );
@@ -27,4 +30,21 @@ window.onload = function () {
 
 function isNotNull(obj) {
     return obj && obj !== 'null' && obj !== 'undefined';
+}
+
+function showToast(msg, err = false) {
+    if (msg != null) {
+        const toast = document.getElementById('toast');
+        const len = msg.length;
+
+        if (err) {
+            toast.classList.add('err');
+        } else {
+            toast.classList.add('ok');
+        }
+
+        toast.innerHTML = msg;
+        toast.classList.add('show', 'animate__fadeInUp');
+        setTimeout(function () { toast.classList.remove('show', 'ok', 'err', 'animate__fadeInUp') }, len * 120);
+    }
 }
