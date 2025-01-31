8c9d15d74f114b8a1e1caf99363fdf1ad9607745
Izaias Lima <izaiasmlima@gmail.com>
2025-01-31 15:04:55 -0300

    Redefinir a ativacao do service-worker

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
 
 	// Ativar o service-worker da página PWA
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/js/service-worker.js')
-		.then(registration => {
-			console.log('Service Worker registrado com sucesso:', registration);
-		})
-		.catch(error => {
-			console.error('Falha ao registrar o Service Worker:', error);
-		});
-	}
+
+	// if ('serviceWorker' in navigator) {
+	// 	navigator.serviceWorker.register('/js/service-worker.js')
+	// 	.then(registration => {
+	// 		console.log('Service Worker registrado com sucesso:', registration);
+	// 	})
+	// 	.catch(error => {
+	// 		console.error('Falha ao registrar o Service Worker:', error);
+	// 	});
+	// }
 	
 
 	/* Hide mobile menu after clicking on a link
